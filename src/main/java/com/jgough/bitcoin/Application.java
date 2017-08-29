8b779195d417cbe120b225fb3e88594aa758c426
James Gough <jpgough@gmail.com>
2017-08-29 17:43:10 +0100

    Remove original code

--- src/main/java/com/jgough/bitcoin/Application.java
+++ src/main/java/com/jgough/bitcoin/Application.java
@@ -24,65 +24,6 @@ public class Application {
     @ResponseBody
     public String calculateBitCoinUSDVar() throws MalformedURLException {
         return calculateBitCoinUSDCustomVar(200);
-//
-//
-//        Calendar end = Calendar.getInstance();
-//        end.add(Calendar.DAY_OF_MONTH, -2); // we substract 2 days because the data may not be updated to latest day
-//
-//        Calendar date = Calendar.getInstance();
-//        date.add(Calendar.YEAR, -1); // we substract depending on the period we want to calculate the volatility for
-//
-//
-//        System.out.println("Begin: " + df.format(date.getTime()));
-//        System.out.println("End: " + df.format(end.getTime()));
-//
-//        try {
-//            // creating the URL using StringBuffer
-//            StringBuffer urlBuffer = new StringBuffer();
-//
-//            URL url = new URL(urlBuffer.toString());
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            String line;
-//            while ((line = reader.readLine()) != null ) {
-//                text.append(line);
-//            }
-//            reader.close();
-//        } catch (IOException e ) {
-//            e.printStackTrace();
-//        }
-//
-//        //parsing the JSON data
-//        JsonParserFactory factory = JsonParserFactory.getInstance();
-//        JSONParser parser = factory.newJsonParser();
-//        Map data = parser.parseJson(text.toString());
-//        Map prices = (Map) data.get("bpi");
-
-        // calculating portfolio data using latest prices
-//        portfolioValue = 200 * Double.parseDouble((String) prices.get(df.format(end.getTime())));
-//
-//        while (date.before(end)) {  // looking up each date in the parsed data and getting the price
-//            dailyBTCPrices.add(Double.parseDouble((String) prices.get(df.format(date.getTime()))));
-//            date.add(Calendar.DAY_OF_MONTH, 1);
-//        }
-//
-//        for (int i = 1; i <= dailyBTCPrices.size() - 1; i++) { // calculating daily return and adding it to the return list
-//            dailyBTCPriceEvolution.add(dailyBTCPrices.get(i)/dailyBTCPrices.get(i-1)*100 - 100);
-//        }
-//
-//        for (Double returnValue : dailyBTCPriceEvolution) { // getting the sum of returns
-//            sumOfReturn += returnValue;
-//        }
-//
-//        averageOfReturn = sumOfReturn/dailyBTCPriceEvolution.size(); // calculating average of returns
-//        std_dev = getStdDev(dailyBTCPriceEvolution, averageOfReturn); // standard deviation using the method
-//        annualVolatility = std_dev * Math.sqrt(365d); // transforming standard deviation in annual volatility
-//        VaR = annualVolatility/100 * Math.sqrt(10/365d) * portfolioValue * 1.65;
-//
-//        Locale usd = new Locale("en","US");
-//        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(usd);
-    //return "";
-        //return "<h3>Value at risk for 200 BTC during next 10 days equals " + currencyFormat.format(VaR) + "</h3>";
     }
 
     @RequestMapping(value = "/", params = {"amount"})
